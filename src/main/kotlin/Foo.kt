b91454478e0468f7b12dd809965db67dde2155bd
Stefan Cross <stefan.cross@sedex.com>
2021-07-15 15:05:34 +0100

    adding our test cases and base class with function for encrypting and decrypting

--- src/main/kotlin/Foo.kt
+++
@@ -1,3 +0,0 @@
-class Foo { val someVal = "someValue" }
-
-fun fooFunction(var1: Int, var2: Int, operation: (Int, Int) -> Int): Int = operation(var1, var2)
---
+++ src/main/kotlin/RailFenceCipher.kt
@@ -0,0 +1,13 @@
+class RailFenceCipher(i: Int) {
+
+    var data = mutableListOf<String>()
+
+    fun getEncryptedData(s: String): String {
+        return s
+    }
+
+    fun getDecryptedData(s: String): Any? {
+        TODO("Not yet implemented")
+    }
+
+}
